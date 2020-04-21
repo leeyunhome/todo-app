f58d221c1723f3af65a844b9c5813d3cd5b4bacc
leeyunhome <leeyunhome@daum.net>
2020-04-22 08:51:40 +0900

    Updates

---
+++ precache-manifest.9e3fe326d8d5c7ed570731bd6657e009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7644ec9e4cceb209abf0888513477f2",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "a47869362c5422f1c225",
+    "url": "/todo-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6ea0d13596d42494263",
+    "url": "/todo-app/static/js/2.26eb6d70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app/static/js/2.26eb6d70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a47869362c5422f1c225",
+    "url": "/todo-app/static/js/main.1fc3811d.chunk.js"
+  },
+  {
+    "revision": "aed0306b7a4838737999",
+    "url": "/todo-app/static/js/runtime-main.3c9e9223.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/todo-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
